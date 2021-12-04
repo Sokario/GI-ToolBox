3d60026d234256cd7d9eb0d4d11602ea20e9bbb2
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-12-04 23:47:39 +0100

    minor fix build.py

--- build.py
+++ build.py
@@ -4,4 +4,4 @@ import subprocess
 if (os.name == "posix"):
     os.system('python3 -m PyInstaller --onefile --add-data "data/img:." ToolBox.pyw')
 elif (os.name == "nt"):
-    subprocess.call(r"python -m PyInstaller --onefile --add-data 'data/img;.' ToolBox.pyw")
+    subprocess.call(r"python -m PyInstaller --onefile --add-data data/img;. ToolBox.pyw")
