1c74bd9de949dc5730c41c7eddbd3269ab440cd8
Sokario <logansaintgermain@gmail.com>
2021-12-04 23:41:40 +0100

    Fix data bundle

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 if (os.name == "posix"):
-    os.system("python3 -m PyInstaller --onefile ToolBox.pyw")
+    os.system('python3 -m PyInstaller --onefile --add-data "data/img:." ToolBox.pyw')
 elif (os.name == "nt"):
-    subprocess.call(r"python -m PyInstaller --onefile ToolBox.pyw")
+    subprocess.call(r"python -m PyInstaller --onefile --add-data 'data/img;.' ToolBox.pyw")
