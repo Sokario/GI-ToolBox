b1892fbd19bea3df61fa3404f3968f0256c3a8e4
Sokario <logansaintgermain@gmail.com>
2021-12-04 22:06:59 +0100

    Fixed onefile compilation

--- build.py
+++ build.py
@@ -4,4 +4,4 @@ import subprocess
 if (os.name == "posix"):
     os.system("python3 -m PyInstaller --onefile ToolBox.pyw")
 elif (os.name == "nt"):
-    subprocess.call(r"python -m PyInstaller ToolBox.pyw")
+    subprocess.call(r"python -m PyInstaller --onefile ToolBox.pyw")
