01fbc15e4f40687d402975798c47cb40e097a33c
Logan <logansaintgermain@gmail.com>
2021-12-04 16:38:41 +0100

    PySimpleGUI example

--- ToolBox.py
+++ ToolBox.py
@@ -1,5 +1,7 @@
-
+import PySimpleGUI as sGUI
 
 if __name__ == "__main__" :
+    sGUI.Window(title = "Genshin Impact ToolBox", layout = [[]], margins = (720, 480)).read()
+
     myInt = 27
     print(f"myInt = {myInt}")
