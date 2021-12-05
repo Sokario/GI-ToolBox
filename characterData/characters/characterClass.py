0da5fdfbed9a46bed013e618de174b92c7b793dd
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-12-05 03:48:28 +0100

    Ajout Character.set_statIncreases

--- characterData/characters/characterClass.py
+++ characterData/characters/characterClass.py
@@ -1,5 +1,5 @@
 #from abc import abstractmethod
-from . import weapons.weaponClass
+from . import weaponClass as wc
 
 class BaseCharacter():
     def __init__(self) :
@@ -18,18 +18,46 @@ class BaseCharacter():
 
         self.constellation = list()
         self.artifacts = dict() #unused for now
-        self.weaponType = BaseWeapon("none", "none") #weapon instance
+        self.weapon = wc.BaseWeapon("none", "none") #weapon instance
 
         self.ascensionMaterials = list()
         self.abilityMaterials   = list()
 
         pass
 
-
     def getStatsAtLevel(self, level):
+        if level > 90:
+            level = 90
+        elif level < 1:
+            level = 0
 
-        pass
+        ret = dict()
+        ret["hp"] = self.baseStats["hp"]
+        ret["atk"] = self.baseStats["atk"]
+        ret["def"] = self.baseStats["def"]
+        ret["crit rate"] = self.baseStats["crit rate"]
+        ret["crit dmg"] = self.baseStats["crit dmg"]
 
+        for i in range(1,level):
+            ret["hp"] += self.statIncreases[i-1]["hp"]
+            ret["atk"] += self.statIncreases[i-1]["atk"]
+            ret["def"] += self.statIncreases[i-1]["def"]
+            ret["crit rate"] += self.statIncreases[i-1]["crit rate"]
+            ret["crit dmg"] += self.statIncreases[i-1]["crit dmg"]
+
+        return ret
 
     def getStats(self):
         return self.getStatsAtLevel(self, self.level)
+
+    def set_statIncreases(self, hpIncreases, atkIncreases, defIncreases, critRateIncreases, critDmgIncreases):
+        self.statIncreases = [
+            {
+                "hp" : hpIncreases[i],
+                "atk" : atkIncreases[i],
+                "def" : defIncreases[i],
+                "crit rate" : critRateIncreases[i],
+                "crit dmg" : critDmgIncreases[i],
+            }
+            for i in range(89)
+        ]
