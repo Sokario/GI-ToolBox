8b778789ff3d4fc253ef71d08bbd3f72b4a721ae
Sokario <logansaintgermain@gmail.com>
2021-12-12 23:04:27 +0100

    Fixed add_data for pyinstaller

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 if (os.name == "posix"):
-    os.system('python3 -m PyInstaller --onefile --add-data "data/img:." ToolBox.pyw')
+    os.system('python3 -m PyInstaller --onefile --add-data "data/img:." --add-data "src:src" ToolBox.pyw')
 elif (os.name == "nt"):
-    subprocess.call(r"python -m PyInstaller --onefile --add-data data\\img;. ToolBox.pyw")
+    subprocess.call(r"python -m PyInstaller --onefile --add-data data\\img;. --add-data src;src ToolBox.pyw")
